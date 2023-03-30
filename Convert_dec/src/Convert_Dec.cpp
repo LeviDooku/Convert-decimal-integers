b128f90f53f9cb913db96af7b2611621fb3865e9
Pedro Velasco Santana <120191699+LeviDooku@users.noreply.github.com>
2023-03-30 18:45:37 +0200

    Update Convert_Dec.cpp

--- Convert_dec/src/Convert_Dec.cpp
+++ Convert_dec/src/Convert_Dec.cpp
@@ -1,4 +1,4 @@
-//convert_dec.cpp
+//Convert_dec.cpp
 
 #include <iostream>
 #include <string>
@@ -7,31 +7,37 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-
+	
+	//Program convert numbers between 0 - 255 (1111 1111)
 	const int TAM_BITS = 8;
 	
+	//Arrays to store final numbers
 	int bin[TAM_BITS];
-	int hex[TAM_BITS];
 	int oct[TAM_BITS];
+	
+	//Generate random integers
 	GeneradorAleatorioEnteros g(0, 255);
 	int n = g.Siguiente(); 
-
+	
+	//Data entry
 	if(argc > 2){
 		cerr << "ERROR: too many arguments" << endl;
 		cerr << "Use: " << "[integer between 0 and 255]";
 		cerr << endl << endl;
 		exit(1);
 	}
-
+	
 	if(argc == 2){
 		n = atoi(argv[1]);
 	}
-
+	
+	//Check the validity of the argument
 	if(n < 0 || n > 255){
 		cerr << "ERROR: integer must be 0 <= n <= 255";
 		exit(2);
 	}
-
+	
+	//By using the functions numbers are shown
 	cout << "Number to convert: " << n;
 
 	Convert_bin(bin, n);
