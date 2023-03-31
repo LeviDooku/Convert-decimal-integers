9cdc1f514cd1b287cbe3475196faa650df9db566
Pedro Velasco Santana <120191699+LeviDooku@users.noreply.github.com>
2023-03-31 20:56:31 +0200

    Update Convert_Declib.h

--- Convert_dec/include/Convert_Declib.h
+++ Convert_dec/include/Convert_Declib.h
@@ -1,4 +1,4 @@
-//convert_declib.h
+//Convert_Declib.h
 
 #include <iostream>
 using namespace std;
@@ -6,9 +6,12 @@ using namespace std;
 #ifndef CONVERT_DECLIB
 #define CONVERT_DECLIB
 
-void Convert_bin(int *bin, int n);
+const int TAM_BITS = 8;
+
+
+void Convert_bin(int *bin, int n=TAM_BITS);
 string Convert_hex(int n);
-void Convert_oct(int *oct, int n);
+void Convert_oct(int *oct, int n=TAM_BITS);
 void ShowArray(const char *msg, int *pv, int n_datos, int datos_linea);
 
 #endif
